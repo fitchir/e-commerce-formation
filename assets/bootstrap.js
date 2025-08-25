683aac85ec49b5254687a74080bcfb12aee40e8c
fatma <itchirfatma@gmail.com>
2025-08-25 23:08:00 +0200

    add user entity, add bootstrap, add webpack encore, add necessary bundle

--- assets/bootstrap.js
+++ assets/bootstrap.js
@@ -1,5 +1,12 @@
-import { startStimulusApp } from '@symfony/stimulus-bundle';
+//import { startStimulusApp } from '@symfony/stimulus-bundle';
 
-const app = startStimulusApp();
+// const app = startStimulusApp();
 // register any custom, 3rd party controllers here
 // app.register('some_controller_name', SomeImportedController);
+
+import { Application } from '@hotwired/stimulus'
+import { definitionsFromContext } from '@hotwired/stimulus-webpack-helpers'
+
+window.Stimulus = Application.start()
+const context = require.context('./controllers', true, /\.js$/)
+Stimulus.load(definitionsFromContext(context))
